bd8d6c97da042361c68989ef0d8ccf0ab72fba1b
Linh Nguyen <linhnguyenhp88@gmail.com>
2017-08-10 00:44:45 +0700

    2017/08/10 Creating Structure project.

---
+++ LinhNguyen.Common/Enum.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinhNguyen.Common
+{
+    public enum UserRole
+    {
+        Admin = 3,
+        Master = 2,
+        Normal = 1
+    }
+
+    public enum UserPaymentMethod
+    {
+
+        Using_Cash = 2,
+        Internet_Banking = 1
+    }
+
+    public enum ProductSize
+    {
+        L = 1,
+        S = 2,
+        M = 3
+    }
+
+    public enum ShippingMethod
+    {
+        QuickDelivery = 1,
+        Standard = 2
+    }
+
+    public enum CategoryType
+    {
+        Dam = 1,
+        Chan_Vay = 2,
+        Quan = 3,
+        Ao = 4,
+        Bo_Suu_Tap = 5,
+        Jumpsuits = 6
+    }
+
+    public enum Gender
+    {
+        Male = 1,
+        Female = 2,
+        Other = 3
+    }
+
+    public enum ProductColor
+    {
+        BEIGE = 1,
+        BLACK = 2,
+        BLUE = 3,
+        BROWN = 4,
+        GOLD = 5,
+        GREEN = 6,
+        GREY = 7,
+        MULTI = 8,
+        NAVY = 9,
+        ORANGE = 10,
+        PINK = 11,
+        PURPLE = 12,
+        RED = 13,
+        SILVER = 14,
+        WHITE = 15,
+        YELLOW = 16
+    }
+
+    public enum SlideShowType
+    {
+        News = 1,
+        Products = 2
+    }
+}
---
+++ LinhNguyen.Domain/Entities/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinhNguyen.Domain.Entities
+{
+    [Table("dbo.User")]
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public virtual long Id { get; set; }
+        public virtual string FullName { get; set; }
+        public virtual string Email { get; set; }
+        public virtual string UserName { get; set; }
+        public virtual string Password { get; set; }
+        public virtual string ConfirmPassword { get; set; }
+        public virtual string Address { get; set; }
+        public virtual string Address1 { get; set; }
+        public virtual string PhoneNumber { get; set; }
+        public virtual DateTime? DateOfBirth { get; set; }
+        public virtual 
+    }
+}

22b37d0b148f13bf7e7be9ee23baa101674c944c
linhnguyenhp88 <linhnguyenhp88@gmail.com>
2017-08-16 18:05:24 +0700

    2017/08/15 Creating entity

--- LinhNguyen.Domain/Entities/Order.cs
+++ LinhNguyen.Domain/Entities/Order.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LinhNguyen.Common;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -15,6 +16,25 @@ namespace LinhNguyen.Domain.Entities
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public long Id { get; set; }
 
-
+        public virtual string OrderCode { get; set; }
+        public virtual long UserId { get; set; }
+        public virtual decimal Total { get; set; }
+        public ICollection<OrderDetail> OrderDetails { get; set; }
+        public User User { get; set; }
+        public bool IsCharged { get; set; }
+        public string CodeDiscount { get; set; }
+        public string Discount { get; set; }
+        public decimal TotalMoney { get; set; }
+        public int Quantity { get; set; }
+        public int PointUsed { get; set; }
+        public bool IsSendToGhn { get; set; }
+        public int ServiceId { get; set; }
+        public double GhnTotalFee { get; set; }
+        public string GhnOrderCode { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string Note { get; set; }
+        public string FullName { get; set; }
+        public UserPaymentMethod PaymentMethod { get; set; }
     }
 }
--- LinhNguyen.Domain/Entities/Product.cs
+++ LinhNguyen.Domain/Entities/Product.cs
@@ -1,13 +1,40 @@
-﻿using System;
+﻿using LinhNguyen.Common;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LinhNguyen.Domain.Entities
 {
+    [Table("dbo.Product")]
     public class Product : BaseClass
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public virtual long ProductId { get; set; }
+        public virtual string Code { get; set; }
+        public virtual string ImagePath1 { get; set; }
+        public virtual string ImagePath2 { get; set; }
+        public virtual string ImagePath3 { get; set; }
+        public virtual string ImagePath4 { get; set; }
+        public virtual string ImagePath5 { get; set; }
 
+        public virtual decimal? Cost { get; set; }
+        public virtual int? Discount { get; set; }
+        public virtual string ProductBy { get; set; }
+        public virtual ProductSize Size { get; set; }
+        public virtual string FreeText { get; set; }
+        public virtual string Description { get; set; }
+        public virtual string AdditionalInformation { get; set; }
+        public virtual int Catetory { get; set; }
+        public virtual string  Modern { get; set; }
+        public virtual string Note { get; set; }
+        public bool IsNewCollection { get; set; }
+        public int? Quantity { get; set; }
+        public int NumLike { get; set; }
+        public ProductColor Color { get; set; }
     }
 }

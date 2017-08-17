c6333b8162180d9142ec3f965f4696d2cf33114f
linhnguyenhp88 <linhnguyenhp88@gmail.com>
2017-08-17 10:05:20 +0700

    20170816 Push Coupons and News Entity

--- LinhNguyen.Domain/Entities/Coupons.cs
+++ LinhNguyen.Domain/Entities/Coupons.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LinhNguyen.Domain.Entities
 {
+    [Table("dbo.Coupon")]
     public class Coupons : BaseClass
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+        public string CouponsCode { get; set; }
+        public string Reason { get; set; }
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+        public string UserNames { get; set; }
+        public string Emails { get; set; }
+        public bool SendEmail { get; set; }
+        public int DiscountPercent { get; set; }
     }
 }
--- LinhNguyen.Domain/Entities/News.cs
+++ LinhNguyen.Domain/Entities/News.cs
@@ -1,13 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LinhNguyen.Domain.Entities
 {
-
+    [Table("dbo.News")]
     public class News : BaseClass
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Paragraph { get; set; }
+        public string ImagePath { get; set; }
     }
 }

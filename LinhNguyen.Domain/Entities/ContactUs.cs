b620c5167557c2541eb074bce2d431d0ee01adff
linhnguyenhp88 <linhnguyenhp88@gmail.com>
2017-08-17 10:46:16 +0700

    20170816 Push ContactUs Entity

--- LinhNguyen.Domain/Entities/ContactUs.cs
+++ LinhNguyen.Domain/Entities/ContactUs.cs
@@ -1,13 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LinhNguyen.Domain.Entities
 {
+    [Table("dbo.ContactUs")]
     public class ContactUs : BaseClass
     {
-
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }      
+        public string FullName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Object { get; set; }
     }
 }
